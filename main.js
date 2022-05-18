86c054a3ee9b7b9b844a43c241471c2eeee68108
0acked <98188838+0acked@users.noreply.github.com>
2022-05-18 16:11:53 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 megalovania = "";
 minecraft = "";
 
@@ -8,11 +13,28 @@ function preload(){
 function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
-
     video = createCapture(VIDEO);
     video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on("pose",gotPoses);
 }
 function draw(){
-    image(0,0,600,500);
+    image(video,0,0,600,500);
     
+}
+function modelLoaded(){
+    console.log("posenet made");
+} 
+function gotPoses(){
+    if(results > 0)
+    {
+        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWrist X = "+leftWristX+" and leftWrist Y = "+leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = "+rightWristX+" and rightWristY = "+rightWristY);
+    }
 }
