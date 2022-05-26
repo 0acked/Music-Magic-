2c10f0d8601c165ca49b0e86981287f9b543c6da
0acked <98188838+0acked@users.noreply.github.com>
2022-05-26 18:26:04 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+leftWristScore = 0;
+rightWrist
 
 megalovania = "";
 minecraft = "";
@@ -15,11 +17,34 @@ function setup(){
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
-    posenet = ml5.poseNet(video, modelLoaded);
-    posenet.on("pose",gotPoses);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
 }
 function draw(){
     image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#FF0000"); 
+
+    if(rightWristScore > 0.2){
+        circle(rightWristX,rightWristY,20);
+        megalovania.stop();
+
+        if(minecraft.isPlaying(false)){
+            minecraft.play();
+            document.getElementById("song").index.HTML = "Playing - Minecraft Music"
+        }
+    }
+
+if (leftWristScore > 0.2)
+{
+    circle(leftWristX,leftWristY,20);
+    InNumberleftWristY = Number(leftWristY);
+    remove_decimals = floor(InNumberleftWristY);
+    volume = remove_decimals/500;
+    document.getElementById("volume").innerHTML = "Volume = " +volume;
+    song.setVolume(volume);
+}
+
     
 }
 function modelLoaded(){
@@ -29,12 +54,15 @@ function gotPoses(){
     if(results > 0)
     {
         
-        leftWristX = results[0].pose.leftWrist.x;
+         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWrist X = "+leftWristX+" and leftWrist Y = "+leftWristY);
 
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
         console.log("rightWristX = "+rightWristX+" and rightWristY = "+rightWristY);
+
+        rightWristScore = results[0].pose.keypoints[10].score;
+        leftWristScore = results[0].pose.keypoints[9].score;
     }
 }
